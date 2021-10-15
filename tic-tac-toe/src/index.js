f522e4d5fab149b8dea68506e205d9a176ff77d9
David Huang <davidhuang1203@hotmail.com>
2021-10-15 15:35:02 +0800

    Create the app & follow part of the tutorial

---
+++ tic-tac-toe/src/index.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+    constructor(props) {
+        // a subclass's constructor should always has a call to super 
+        // all react components constructor should call super(props)
+        super(props);
+        this.state = {value: null};
+    }
+
+    
+
+    render() {
+      return (
+          // like here in the onClick, for the event handler in react, 
+          // its a good practice to use arrow func to avoid problems with closure
+        <button className="square" onClick={()=>this.setState({value: 'X'})}>
+          {this.state.value}
+        </button>
+      );
+    }
+  }
+  
+  class Board extends React.Component {
+    renderSquare(i) {
+      return <Square value={i} />;
+    }
+  
+    render() {
+      const status = 'Next player: X';
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
