bd7a3d375aee87f46193cf994b4db97bc05f7531
David Huang <davidhuang1203@hotmail.com>
2021-10-20 20:41:52 +0800

    Follow through the tutorial

--- tic-tac-toe/src/TutorialGame.js
+++ tic-tac-toe/src/TutorialGame.js
@@ -2,14 +2,18 @@ import React from "react";
 
 
 // plan:
-// Square: display the right player
-// Board:
-// track of state of each square
-// 	pass on the state info to each square
-// determine player
-// check winner
+// lifting the state into game
+	// pass necessary infos via props
+	// lift handle click
 
+// add history, replace squares with current most history
+	// use step to display history
+	// calc the next player based on the step
+// display history, with each step name
+// go back to history
+	// change the step to go back
 
+// controlled component 
 function Square(props){
 	return (<button className="square" 
 					onClick={props.onClick}>
@@ -18,61 +22,15 @@ function Square(props){
 }
 
 class Board extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			// use an array to track of squares, initialize with null
-			squares: new Array(9).fill(null),
-			// use a bool to det player
-			isXNext: true,
-		}
-	}
-
 	renderSquare(position) {
 		return (<Square 
-							value={this.state.squares[position]} 
-							onClick={()=>{this.handleClick(position)}}/>);
-	}
-
-	handleClick(position) {
-		const {squares, isXNext} = this.state;
-
-		// if square has already been occupied/game won, return
-		if 
-		(squares[position]
-			|| calcWinner(squares)
-		) return;
-
-		// get the player
-		const player = isXNext? 'X': 'O';
-		this.setState({isXNext: !isXNext})
-
-		// change the squares array
-		// notice： with const its still possible to change properties of 
-		// an obj. but cannot reassign the var to a new obj.
-		squares[position] = player;
-		this.setState({squares: squares})
+							value={this.props.squares[position]} 
+							onClick={()=>{this.props.onClick(position)}}/>);
 	}
 
 	render() {
-		const {squares, isXNext} = this.state;
-
-		// check for winner each time its re-rendered
-		const winner = calcWinner(squares);
-		let status;
-		if(winner)
-		{
-			status = `Winner: ${winner}`;
-		}
-		else
-		{
-			const player = isXNext? 'X': 'O';
-			status = `Next player: ${player}`;
-		}
-
 		return (
 			<div>
-				<div className="status">{status}</div>
 				<div className="board-row">
 					{this.renderSquare(0)}
 					{this.renderSquare(1)}
@@ -94,15 +52,109 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			// track of history
+			history: [Array(9).fill(null),],
+
+			// we can deduce the player from the step number
+			step: 0,
+
+		};
+	}
+
+	// helper function for getting player based on the step
+	getPlayer = () => (this.state.step % 2 === 0)? 'X': 'O';
+
+	// TODO the tutorial didn't use arrow function but still works, emmm... weird
+	// each click -> step ++, new array added to history
+	handleClick = (position) => {
+		const {history, step} = this.state;
+		
+		// determine player
+		const player = this.getPlayer();
+
+		// make sure we dont mutate the state!
+		const squares = history[step].slice();
+		// if square has already been occupied/game won, return
+		if (squares[position]
+			|| calcWinner(squares)
+		) return;
+
+		// change the squares array
+		// notice： with const its still possible to change properties of 
+		// an obj. but cannot reassign the var to a new obj.
+		squares[position] = player;
+
+		// add the squares to history, step + 1
+		this.setState({
+			history: history.slice(0, step+1).concat([squares]),
+			step: step+1,
+		});
+	};
+
+	renderHistory(){
+		const moveHistory = this.state.history.map(
+			(_, step) => {
+				let styling, text;
+				if (step===0) {
+					styling = {listStyleType: "none"};
+					text = "Go to game start";
+				} else {
+					styling = null;
+					text = `Go to step ${step}`
+				}
+
+				return (
+				<li key={step} style={styling}>
+					<button  onClick={()=>this.jumpTo(step)}> 
+						{text}
+					</button>
+				</li>);
+			}
+		)
+
+		return moveHistory;
+	}
+
+	jumpTo(stepTo) {
+		// change the step
+		console.log('clicked');
+		this.setState({step: stepTo});
+	}
+
 	render() {
+		console.log(this.state.step);
+		const {history, step} = this.state;
+		const squares = history[step];
+
+		// determine player
+		const player = this.getPlayer();
+
+		// check for winner each time its re-rendered
+		const winner = calcWinner(squares);
+		let status;
+		if(winner)
+		{
+			status = `Winner: ${winner}`;
+		}
+		else
+		{
+			status = `Next player: ${player}`;
+		}
+
 		return (
 			<div className="game">
 				<div className="game-board">
-					<Board />
+					<Board 
+					onClick={this.handleClick} 
+					squares={squares}/>
 				</div>
 				<div className="game-info">
-					<div>{/* status */}</div>
-					<ol>{/* TODO */}</ol>
+					<div>{status}</div>
+					<ol start={0}>{this.renderHistory()}</ol>
 				</div>
 			</div>
 		);
@@ -112,6 +164,7 @@ class Game extends React.Component {
 // helper function, returns the winner of the game
 // using helper function to keeps the code within the class minimal
 function calcWinner(squares){
+
 	const WINCASES = [
 		[0, 1, 2],
     [3, 4, 5],
